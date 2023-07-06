4ecdf7ea9f17ac6ed1f38e5dd776709c5a1b3c47
adamdervisevic <adamdervisevic@yahoo.com>
2023-07-06 10:50:25 +0200

    sa casa

--- videoteka/app/Models/Person.php
+++ videoteka/app/Models/Person.php
@@ -4,12 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Person extends Model
 {
     use HasFactory;
 
-    //polja u tabeli, dodato
-    protected $fillable = ['id', 'name', 'surname', 'b_date'];
-
+    protected function fullName(): Attribute{
+        return Attribute::make(
+            get: fn () => ($this->name . " " . $this->surname)
+            ,
+        );
+    }
 }
